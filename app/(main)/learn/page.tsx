b50b305907341d1bac6bda3b77e8bc0d32a15df8
Christian Stief <chrstief@gmail.com>
2024-11-24 09:52:03 +0100

    Units

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,14 +1,18 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Header } from "./header";
 
 export default async function LearnPage() {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress?.activeCourse) redirect("/courses");
 
@@ -24,6 +28,11 @@ export default async function LearnPage() {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
