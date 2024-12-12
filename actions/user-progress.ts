b4e0a780ee631920c987a08fbd736f32202c1697
Christian Stief <chrstief@gmail.com>
2024-12-12 14:50:04 +0100

    Shop

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -8,6 +8,8 @@ import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const POINTS_TO_REFILL = 10;
+
 export async function upsertUserProgress(courseId: number) {
   const { userId } = await auth();
   const user = await currentUser();
@@ -90,4 +92,29 @@ export async function reduceHearts(challengeId: number) {
   revalidatePath("/quests");
   revalidatePath("/leaderboards");
   revalidatePath(`/lesson/${lessonId}`);
+}
+
+export async function refillHearts() {
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) throw new Error("User progress not found");
+
+  if (currentUserProgress.hearts === 5)
+    throw new Error("Hearts are already full");
+
+  if (currentUserProgress.points < POINTS_TO_REFILL)
+    throw new Error("Not enough points");
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: 5,
+      points: currentUserProgress.points - POINTS_TO_REFILL,
+    })
+    .where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboards");
 }
